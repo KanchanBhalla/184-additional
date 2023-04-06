8b8157787ec420e1c36da9aa3c93fe72b5032ba5
Kanchan Bhalla <50230249+KanchanBhalla@users.noreply.github.com>
2023-04-06 13:50:02 +0530

    Create usb.py

---
+++ usb.py
@@ -0,0 +1,31 @@
+from tkinter import *
+import subprocess,json
+root = Tk()
+root.geometry("500x500")
+out = subprocess.getoutput("powershell -Command\" & {Get-PnpDevice / Select-ObjectStatus, Class, FriendlyName / ConvertTo-Json}\"")
+e1 = Label(root, text = " Status ", font= ('Helvatika',10,'bold'))
+e1.grid(row = 0, column = 1, padx = 5, pady = 5)
+e1 = Label(root, text = " Class ", font= ('Helvatika',10,'bold'))
+e1.grid(row = 0, column = 2, padx = 5, pady = 5)
+
+count = 1
+print(out)
+json_data = json.loads(out)
+
+for dev in json_data:
+    if count <= 13:
+        lst = [[dev['Status'],dev]['Class'], dev["FriendlyName"]]
+        total_rows = len(lst) 
+        total_columns = len(lst[0])
+ 
+        for i in range(total_rows):
+            for j in range(total_columns):
+                
+                e = Entry(root, width= 35, fg = " blue " )
+                e.grid(row= count, column = j, ipadx = 5, ipady = 5, columnspan = 2)
+                e.insert(END, lst[i][j])
+                
+        count = count + 1
+        print( count )
+rppt.mainloop()                  
+                           
